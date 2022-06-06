5d66fdd9af6cb5c864a3ccd45af4e2d996f4990a
LaOngDaoxing <18731197300@163.com>
2022-06-06 14:30:08 +0800

    开发人员|测试人员|收款者手机微信的openid

--- src/com/ljx/wamt/wxutils/WechatPayUtil.java
+++ src/com/ljx/wamt/wxutils/WechatPayUtil.java
@@ -112,9 +112,9 @@ public class WechatPayUtil {
         // 商户号
         wechatPayReqDTO.setMchid("1372809402");
         // 商户名称
-        wechatPayReqDTO.setMch_name("商户名称");
+        wechatPayReqDTO.setMch_name("某某商户名称");
         // 商户appid下，绑定的某微信用户的openid；此微信用户零钱将收款3毛
-        wechatPayReqDTO.setOpenid("开发|测试人员|收款者手机微信的openid");
+        wechatPayReqDTO.setOpenid("开发人员|测试人员|收款者手机微信的openid");
         // 企业付款金额，这里单位为元
         wechatPayReqDTO.setAmount(0.3);
         // 微信商户appkey
